d040c62cf3f319042d0730a481a8fdb74277f092
Devika18 <devikankarpe@gmail.com>
2022-12-22 00:39:02 +0530

    added: ability to display employee details through name

--- src/bridgelabz/EmployeePayrollService.java
+++ src/bridgelabz/EmployeePayrollService.java
@@ -10,21 +10,16 @@ public class EmployeePayrollService {
         String userName = "root";
         String password = "password";
         Connection connection = null;
-        double basicPay = 400000.00;
-        int id = 1;
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection done!!!");
-            String query = "UPDATE employee_payroll SET basicPay=? WHERE id=?";
-            PreparedStatement prestatement = connection.prepareStatement(query);
-            prestatement.setDouble(1, basicPay);
-            prestatement.setInt(2, id);
-            prestatement.executeUpdate();
-
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employee_payroll WHERE name = ?");
+            preparedStatement.setString(1, "Ashwini");
+            ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("gender") + " | " + resultSet.getString("phone_number") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getDouble("basicPay") + " | " + resultSet.getDouble("deductions") + " | " + resultSet.getDouble("taxablePay") + " | " + resultSet.getDouble("netPay") + " | " + resultSet.getDouble("incomeTax") + " | " + resultSet.getDate("start"));
             }
