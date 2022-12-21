7be4674c760ba207d4909f95131f46e936e4efef
Devika18 <devikankarpe@gmail.com>
2022-12-22 00:35:36 +0530

    added: logic for updating salary using prepared statement

--- src/bridgelabz/EmployeePayrollService.java
+++ src/bridgelabz/EmployeePayrollService.java
@@ -4,17 +4,25 @@ import java.sql.*;
 
 public class EmployeePayrollService {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Status of connecting database to java code: ");
+        System.out.println("Connecting database to java code: ");
 
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
         String userName = "root";
         String password = "password";
         Connection connection = null;
+        double basicPay = 400000.00;
+        int id = 1;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded!");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
-            System.out.println("Connection done!!");
+            System.out.println("Connection done!!!");
+            String query = "UPDATE employee_payroll SET basicPay=? WHERE id=?";
+            PreparedStatement prestatement = connection.prepareStatement(query);
+            prestatement.setDouble(1, basicPay);
+            prestatement.setInt(2, id);
+            prestatement.executeUpdate();
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
             while (resultSet.next()) {
