2778405c848fe35154e1e5a0ab4ecd08f1e81c45
Devika18 <devikankarpe@gmail.com>
2022-12-22 00:18:24 +0530

    added: logic for retrieving data

--- src/bridgelabz/EmployeePayrollService.java
+++ src/bridgelabz/EmployeePayrollService.java
@@ -1,12 +1,10 @@
 package bridgelabz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class EmployeePayrollService {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Connecting database to java code");
+        System.out.println("Status of connecting database to java code: ");
 
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
         String userName = "root";
@@ -14,9 +12,14 @@ public class EmployeePayrollService {
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Driver loaded");
+            System.out.println("Driver loaded!");
             connection = DriverManager.getConnection(jdbcURL, userName, password);
-            System.out.println("Connection done!");
+            System.out.println("Connection done!!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_payroll");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("id") + " | " + resultSet.getString("name") + " | " + resultSet.getString("gender") + " | " + resultSet.getString("phone_number") + " | " + resultSet.getString("address") + " | " + resultSet.getString("department") + " | " + resultSet.getDouble("basicPay") + " | " + resultSet.getDouble("deductions") + " | " + resultSet.getDouble("taxablePay") + " | " + resultSet.getDouble("netPay") + " | " + resultSet.getDouble("incomeTax") + " | " + resultSet.getDate("start"));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
